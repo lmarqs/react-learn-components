49432fd3b9cab25954075239410d437f0106bf04
Lucas Marques de Paula <lmdp.cartoes@scopus.com.br>
2019-05-22 16:25:37 -0300

    build vue

---
+++ docs/precache-manifest.a1b5ff19051eb813fc51b4f7a203122a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e03990067eb0a88646dc03acefb00208",
+    "url": "/react-learn-components/index.html"
+  },
+  {
+    "revision": "026e2d55bd07739be56c",
+    "url": "/react-learn-components/static/css/main.65727c86.chunk.css"
+  },
+  {
+    "revision": "a5fe96a0868ddb444e80",
+    "url": "/react-learn-components/static/js/2.d23f77b2.chunk.js"
+  },
+  {
+    "revision": "026e2d55bd07739be56c",
+    "url": "/react-learn-components/static/js/main.7c82f0c9.chunk.js"
+  },
+  {
+    "revision": "5f45aba957bda7eb9c91",
+    "url": "/react-learn-components/static/js/runtime~main.a686f51e.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.d9f1be13f17ead10d1080e7c929500ae.js"
+  "/react-learn-components/precache-manifest.a1b5ff19051eb813fc51b4f7a203122a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-learn-components/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
