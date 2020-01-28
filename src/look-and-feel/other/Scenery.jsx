a8e49ef221c76d55d1b4be93cd29c5624cbdbcbc
lmarqs <marques.analista@gmail.com>
2020-01-28 03:14:06 -0300

    add sun and moon

--- src/look-and-feel/other/Scenery.jsx
+++ src/look-and-feel/other/Scenery.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 
 import useIsNight from 'hooks/useIsNight';
 
+import sun from './sun.jpg';
+import moon from './moon.jpg';
+
+const style = {
+  height: "100%",
+};
+
 const Scenery = () => {
   const { isNight, toggleIsNight } = useIsNight();
 
   return (
-    <div>
-      <button onClick={toggleIsNight}>Toggle</button>
-      <br />
-      <dl>
-        <dd>isNight:</dd>
-        <dd>{ isNight.toString() }</dd>
-      </dl>
-    </div>
+    <div style={{ ...style, backgroundImage: `url(${isNight ? moon : sun})` }} onClick={toggleIsNight} />
   )
 }
 
