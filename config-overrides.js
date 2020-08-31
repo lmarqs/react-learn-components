8840f217abe305df72cb3755c4c4df56fa3f471a
lmarqs <marques.analista@gmail.com>
2020-08-31 15:19:44 -0300

    add rewiremock base config

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,11 @@
+const path = require('path');
+
 module.exports = {
     webpack(config, env) {
+        if (env !== 'production') {
+            config.entry.unshift(path.join(process.cwd(), 'support/rewiremock'));
+            config.plugins.push(new require('rewiremock/webpack/plugin'));
+        }
         return config
     }
 }
