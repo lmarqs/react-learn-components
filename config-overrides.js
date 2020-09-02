eefee8ca225166564a3c1a098a701a02379e717a
lmarqs <marques.analista@gmail.com>
2020-09-02 17:17:20 -0300

    fix

--- config-overrides.js
+++ config-overrides.js
@@ -4,7 +4,7 @@ module.exports = {
     webpack(config, env) {
         if (env !== 'production') {
             config.entry.unshift(path.join(process.cwd(), 'support/rewiremock'));
-            config.plugins.push(new require('rewiremock/webpack/plugin'));
+            config.plugins.push(new (require('rewiremock/webpack/plugin'))());
         }
         return config
     }
